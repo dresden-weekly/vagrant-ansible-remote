0d18841c9212af96ab28c5dcac4b0b70ea81b769
JonathanS <mail@strfry.org>
2018-06-21 20:29:48 +0200

    Update default Ansible version in template to 2.5.4

--- template/ansible/install.sh
+++ template/ansible/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #   Version of Ansible that is used
-ANSIBLE_VERSION=${ANSIBLE_VERSION:=1.8.2}
+ANSIBLE_VERSION=${ANSIBLE_VERSION:=2.5.4}
 
 source $PROJECT_FOLDER/$VAGRANT_ANSIBLE_REMOTE/ansible/git-install.sh
